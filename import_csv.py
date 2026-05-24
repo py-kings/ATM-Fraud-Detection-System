33b9e7bdb8cc7019ea2ac89d23130509fff1bd50
py-kings <92917636+py-kings@users.noreply.github.com>
2026-05-24 20:30:50 +0530

    Import CSV data into MySQL database
    This script imports credit card transaction data from a CSV file into a MySQL database, specifically into the 'fraud_data' table. It connects to the database, iterates through the DataFrame, and executes an insert statement for each row.

--- import_csv.py
+++ import_csv.py
@@ -1,1 +1,39 @@
+--import file
 
+import pandas as pd
+import mysql.connector
+
+df = pd.read_csv(r"C:\Users\samad\Downloads\creditcard.csv")
+
+df = df[['Time', 'Amount', 'Class']]
+
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="YOUR_PASSWORD",
+    database="atm_fraud"
+)
+
+cursor = conn.cursor()
+
+for _, row in df.iterrows():
+
+    sql = """
+    INSERT INTO fraud_data (Time, Amount, Class)
+    VALUES (%s, %s, %s)
+    """
+
+    values = (
+        float(row['Time']),
+        float(row['Amount']),
+        int(row['Class'])
+    )
+
+    cursor.execute(sql, values)
+
+conn.commit()
+
+print("Data Imported Successfully")
+
+cursor.close()
+conn.close()
